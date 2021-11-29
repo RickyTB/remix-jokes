69624d03189f1974e59c50549e070f06f3eb5641
Ricardo Baquero <wola_ricky@hotmail.com>
2021-11-29 02:05:26 -0500

    - DB section finished.

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,7 +1,27 @@
-interface JokeRouteProps {}
+import type { LoaderFunction } from "remix";
+import { Link, useLoaderData } from "remix";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
 
-const JokeRoute: React.FC<JokeRouteProps> = () => {
-  return <div>Joke Id</div>;
+type LoaderData = { joke: Joke };
+
+export let loader: LoaderFunction = async ({ params }) => {
+  let joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+  if (!joke) throw new Error("Joke not found");
+  let data: LoaderData = { joke };
+  return data;
 };
 
-export default JokeRoute;
+export default function JokeRoute() {
+  let data = useLoaderData<LoaderData>();
+
+  return (
+    <div>
+      <p>Here's your hilarious joke:</p>
+      <p>{data.joke.content}</p>
+      <Link to=".">{data.joke.name} Permalink</Link>
+    </div>
+  );
+}
